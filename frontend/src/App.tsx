ef9922a8e47510fd426268f44a1fe56243b965d2
antonin <r.antonin.pro@gmail.com>
2026-09-22 17:07:23 +0200

    Branche les routes protégées collection et stats

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,19 +5,29 @@ import RegisterPage from './pages/auth/register.tsx'
 import RecipesPage from './pages/recipes/recipes.tsx'
 import MyRecipesPage from './pages/recipes/my-recipes.tsx'
 import NavBar from './components/NavBar'
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
+import CollectionPage from "./pages/collection/collection.tsx";
+import StatsPage from "./pages/stats/stats.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
 
 function App() {
   return (
-      <BrowserRouter>
-          <NavBar />
+      <AuthProvider>
+          <BrowserRouter>
+              <NavBar />
 
-          <Routes>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/recipes" element={<RecipesPage />} />
-              <Route path="/recipes/mine" element={<MyRecipesPage />} />
-          </Routes>
-      </BrowserRouter>
+              <Routes>
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/register" element={<RegisterPage />} />
+                  <Route path="/recipes" element={<RecipesPage />} />
+                  <Route path="/recipes/mine" element={<MyRecipesPage />} />
+                  <Route element={<ProtectedRoute />}>
+                      <Route path="/collection" element={<CollectionPage />} />
+                      <Route path="/stats" element={<StatsPage />} />
+                  </Route>
+              </Routes>
+          </BrowserRouter>
+      </AuthProvider>
   )
 }
 
